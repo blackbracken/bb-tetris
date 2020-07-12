df4e3d400a594c3863c5106d63d392eaef672c38
blackbracken <me@bracken.black>
2020-07-12 20:06:05 +0900

    prevent mino to move beyond wall

--- scene/marathon.c
+++ scene/marathon.c
@@ -5,7 +5,9 @@
 #include <time.h>
 
 #include "../graphics.h"
-#include "../utils.h"
+
+#define FIELD_WIDTH 10
+#define FIELD_HEIGHT 21
 
 typedef enum {
     RED,
@@ -26,7 +28,7 @@ typedef struct {
 } Tetrimino;
 
 typedef struct {
-    MinoBlock field[20][10];
+    MinoBlock field[FIELD_HEIGHT][FIELD_WIDTH];
     Tetrimino *dropping_mino;
     int dropping_mino_x, dropping_mino_y;
     int dropping_mino_spin;
@@ -42,9 +44,9 @@ Tetrimino MINO_T = {
         .center_y =  1,
         .shape = {
                 {
-                        {0, 0, 0},
-                        {1, 1, 1},
                         {0, 1, 0},
+                        {1, 1, 1},
+                        {0, 0, 0},
                 },
                 {
                         {0, 1, 0},
@@ -52,9 +54,9 @@ Tetrimino MINO_T = {
                         {0, 1, 0},
                 },
                 {
-                        {0, 1, 0},
-                        {1, 1, 1},
                         {0, 0, 0},
+                        {1, 1, 1},
+                        {0, 1, 0},
                 },
                 {
                         {0, 1, 0},
@@ -65,9 +67,9 @@ Tetrimino MINO_T = {
         .size = 3,
 };
 
-void draw_field(int y, int x);
+void draw_board_frame(int field_orig_y, int field_orig_x);
 
-void erase_background_of_field(int y, int x);
+void erase_background_of_field(int orig_y, int orig_x);
 
 int can_move(Board *board, void (*predicate)(Board *));
 
@@ -89,16 +91,21 @@ int can_move(Board *board, void (*predicate)(Board *)) {
 
     for (int j = 0; j < assumed.dropping_mino->size; j++) {
         for (int i = 0; i < assumed.dropping_mino->size; i++) {
-            if (assumed.dropping_mino->shape[assumed.dropping_mino_spin][j][i] == 0) continue;
+            if (assumed.dropping_mino->shape[assumed.dropping_mino_spin][j][i] == 0) {
+                continue;
+            }
+            int dest_x = assumed.dropping_mino_x + (i - assumed.dropping_mino->center_x);
+            int dest_y = assumed.dropping_mino_y + (j - assumed.dropping_mino->center_y);
 
-            int x = assumed.dropping_mino_x + (i - assumed.dropping_mino->center_x);
-            int y = assumed.dropping_mino_y + (j - assumed.dropping_mino->center_y);
-
-            if (x < 0 || y <= 0 || 10 <= x || 20 <= y || assumed.field[y][x] != 0) return 0;
+            if (dest_x < 0 || dest_y < 0
+                || FIELD_WIDTH <= dest_x || FIELD_HEIGHT <= dest_y
+                || assumed.field[dest_y][dest_x] != 0) {
+                return false;
+            }
         }
     }
 
-    return 1;
+    return true;
 }
 
 void move_left(Board *board) { board->dropping_mino_x--; }
@@ -123,24 +130,26 @@ void spin_left(Board *board) {
 
 
 _Noreturn void start_marathon(int lines) {
-    int frame = 1;
-    int board_x = WINDOW_WIDTH / 2 - 12, board_y = 10;
-
-    struct timespec timespec;
-
-    efface_window();
-    draw_window_frame();
-    draw_field(board_y, board_x);
+    int field_orig_x = WINDOW_WIDTH / 2 - 12;
+    int field_orig_y = 10;
+    { // render a view
+        efface_window();
+        draw_window_frame();
+        draw_board_frame(field_orig_y, field_orig_x);
+        erase_background_of_field(field_orig_y, field_orig_x);
+    }
 
     Board board = {
-            .field = {},
+            .field = {0},
             .dropping_mino = &MINO_T,
-            .dropping_mino_x = 5,
-            .dropping_mino_y = 0,
+            .dropping_mino_x = 4,
+            .dropping_mino_y = 2,
             .dropping_mino_spin = 0,
             .dropping_mass_per_second = 1,
     };
 
+    int frame = 1;
+    struct timespec timespec;
     while (1) {
         timespec_get(&timespec, TIME_UTC);
         long milli_start = timespec.tv_nsec / 1000 / 1000;
@@ -155,13 +164,13 @@ _Noreturn void start_marathon(int lines) {
                 if (can_move(&board, drop_softly)) drop_softly(&board);
             }
 
-            erase_background_of_field(board_y, board_x);
+            erase_background_of_field(field_orig_y, field_orig_x);
 
-            // render a board
-            for (int y = 0; y < 20; y++) {
-                for (int x = 0; x < 10; x++) {
-                    if (board.field[y][x]) {
-                        move(board_y + y, board_x + 2 * x);
+            // render a board - unused yet
+            for (int j = 0; j < FIELD_HEIGHT; j++) {
+                for (int i = 0; i < FIELD_WIDTH; i++) {
+                    if (board.field[j][i]) {
+                        move(field_orig_y + j, field_orig_x + 2 * i);
                         attrset(COLOR_PAIR(COLOR_ID_MINO_T));
                         addstr("  ");
                     }
@@ -169,12 +178,12 @@ _Noreturn void start_marathon(int lines) {
             }
 
             // render a dropping mino
-            for (int y = 0; y < board.dropping_mino->size; y++) {
-                for (int x = 0; x < board.dropping_mino->size; x++) {
-                    if (board.dropping_mino->shape[board.dropping_mino_spin][y][x]) {
+            for (int j = 0; j < board.dropping_mino->size; j++) {
+                for (int i = 0; i < board.dropping_mino->size; i++) {
+                    if (board.dropping_mino->shape[board.dropping_mino_spin][j][i]) {
                         move(
-                                board_y + board.dropping_mino_y + board.dropping_mino->center_y - y,
-                                board_x + 2 * (board.dropping_mino_x - board.dropping_mino->center_x + x) + 2
+                                field_orig_y + board.dropping_mino_y + (j - board.dropping_mino->center_y),
+                                field_orig_x + 2 * (board.dropping_mino_x + (i - board.dropping_mino->center_x))
                         );
                         attrset(COLOR_PAIR(COLOR_ID_MINO_T));
                         addstr("  ");
@@ -234,38 +243,36 @@ _Noreturn void start_marathon(int lines) {
     }
 }
 
-void draw_field(int y, int x) {
+void draw_board_frame(int field_orig_y, int field_orig_x) {
     // draw walls
-    for (int i = 0; i < 20; i++) {
-        move(y + i, x);
+    for (int i = 0; i < FIELD_HEIGHT; i++) {
+        move(field_orig_y + i, field_orig_x - 2);
         attrset(COLOR_PAIR(COLOR_ID_FIELD));
         addstr("[]");
 
-        move(y + i, x + 20 + 2);
+        move(field_orig_y + i, field_orig_x + 2 * FIELD_WIDTH);
         attrset(COLOR_PAIR(COLOR_ID_FIELD));
         addstr("[]");
     }
 
     // draw bottom
-    for (int i = 0; i < 20 + 4; i++) {
-        move(y + 20, x + i);
+    for (int i = -2; i < 2 * FIELD_WIDTH + 2; i++) {
+        move(field_orig_y + FIELD_HEIGHT, field_orig_x + i);
         attrset(COLOR_PAIR(COLOR_ID_FIELD));
         addch('T');
     }
-
-    erase_background_of_field(y, x);
 }
 
-void erase_background_of_field(int y, int x) {
-    for (int j = -2; j < 20; j++) {
-        for (int i = 0; i < 10; i++) {
-            int should_be_period = i % 2 == 0 && j % 2 == 0 && j >= 0;
+void erase_background_of_field(int orig_y, int orig_x) {
+    for (int j = 0; j < FIELD_HEIGHT; j++) {
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            int should_be_period = i % 2 == 0 && j % 2 == 0;
 
-            move(y + j, x + i + 2);
+            move(orig_y + j, orig_x + i);
             attrset(COLOR_PAIR(COLOR_ID_FIELD));
             addch(should_be_period ? '.' : ' ');
 
-            move(y + j, x + i + 2 + 10);
+            move(orig_y + j, orig_x + i + 10);
             attrset(COLOR_PAIR(COLOR_ID_FIELD));
             addch(should_be_period ? '.' : ' ');
         }
